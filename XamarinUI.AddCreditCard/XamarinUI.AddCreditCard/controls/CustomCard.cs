ea2d9d1c299d13303b17ad8e4fe9ba6e0f37f8ae
Alexandre Lima <alexandreslima@outlook.com>
2020-03-11 18:45:10 -0300

    add custom card

---
+++ XamarinUI.AddCreditCard/XamarinUI.AddCreditCard/controls/CustomCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinUI.AddCreditCard.controls
+{
+    public class CustomCard : Frame
+    {
+        public CustomCard()
+        {
+            HasShadow = !(Device.RuntimePlatform == Device.iOS);
+            CornerRadius = 20;
+            BackgroundColor = (Color)App.Current.Resources.FirstOrDefault(x => x.Key.Equals("accent")).Value;
+        }
+    }
+}
