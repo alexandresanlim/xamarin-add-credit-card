28765ba010d6a2b70fad09cd6cadca2bab021d63
Alexandre Lima <alexandreslima@outlook.com>
2021-01-18 17:30:36 -0300

    improvements

--- XamarinUI.AddCreditCard/XamarinUI.AddCreditCard.Android/MainActivity.cs
+++ XamarinUI.AddCreditCard/XamarinUI.AddCreditCard.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms.Platform.Android;
 
 namespace XamarinUI.AddCreditCard.Droid
 {
@@ -16,6 +17,8 @@ namespace XamarinUI.AddCreditCard.Droid
         {
             //TabLayoutResource = Resource.Layout.Tabbar;
             //ToolbarResource = Resource.Layout.Toolbar;
+
+            this.SetStatusBarColor(Xamarin.Forms.Color.FromHex("#ecf0f1").ToAndroid());
 
             base.OnCreate(savedInstanceState);
 
